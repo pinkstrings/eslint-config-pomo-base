4f10b230dd97cfa5319363676c29d22de33b0f06
Tom Adam Shani <tom@tomshani.com>
2017-08-01 03:05:23 -0400

    Add some rule changes

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ module.exports = {
     'airbnb-base'
   ],
   rules: {
+    'arrow-parens': [2, 'always'],
     'comma-dangle': [2, 'never'],
+    'max-len': [2, 120, 2],
+    'no-unused-vars': [2, { vars: 'local', args: 'none' }],
+    'no-restricted-syntax': [2, 'DebuggerStatement', 'ForInStatement', 'LabeledStatement', 'WithStatement'],
     'semi': [2, 'never']
   }
 }
